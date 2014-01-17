0eb1fe41bdce756ad348718d21be0cebf69778fc
wroy <wroy@uwo.ca>
2014-01-17 17:05:44 -0500

    made some sick programmer art menus

---
+++ Assets/scripts/MenuScripts/IntroMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroMenuScript : MonoBehaviour {
+
+	//public GUISkin skin;//button gui skin
+	
+	void Update () { 
+		//transform.Rotate(new Vector3(0.0f, rotationAmount, 0.0f)); //rotate the camera arround the map
+	}
+	
+	void OnGUI(){
+		//GUI.skin = skin;
+		
+		//set demensions of the buttons
+		float buttonW = 100;
+		float buttonH = 50;
+		
+		//set the play game button
+		float halfScreenW = (Screen.width/2) -50;
+		float halfScreenH = Screen.height/2 + 0;
+		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Play Game")){
+			Application.LoadLevel("ModeSelectMenu");
+		}
+		
+		//set the exit button
+		halfScreenW = (Screen.width/2) - 50;
+		halfScreenH = Screen.height/2 + 60;
+		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Exit")){
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/scripts/MenuScripts/LevelSelectUserScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectUserScript : MonoBehaviour {
+
+	/*********************************************************
+	 * Application Loads for this script need to be completed
+	 * once those game types begin implementation.
+	 */ 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnGUI () {
+		//set demensions of the buttons
+		float buttonW = 100;
+		float buttonH = 50;
+		
+		//set the play user button
+		float halfScreenW = (Screen.width/2) - 185;
+		float halfScreenH = Screen.height/2 + 0;
+		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW+30,buttonH),"Play User Made")){
+			Application.LoadLevel("game");
+		}
+		
+		//set the build your own button
+		halfScreenW = (Screen.width/2) + 55;
+		halfScreenH = Screen.height/2 + 0;
+		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW+30,buttonH),"Build Your Own")){
+			Application.LoadLevel("game");
+		}
+		
+		//set the back button
+		halfScreenW = (Screen.width/2) - 50;
+		halfScreenH = Screen.height/2 + 60;
+		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back")){
+			Application.LoadLevel("ModeSelectMenu");
+		}
+	}
+}
