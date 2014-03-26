86946c27a4039ae2e251e4214839bb3855ddf949
wroy <wroy@uwo.ca>
2014-03-26 18:30:04 -0400

    adding play next level in post game screen

--- Assets/scripts/MenuScripts/PostGameScript.cs
+++ Assets/scripts/MenuScripts/PostGameScript.cs
@@ -14,30 +14,71 @@ public class PostGameScript : MonoBehaviour {
 	
 	void OnGUI () {
 		//set demensions of the buttons
-		float buttonW = 100;
-		float buttonH = 50;
-		
-		//set the play game button
-		float halfScreenW = (Screen.width/2) - 50;
-		float halfScreenH = Screen.height/2 + 0;
-		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Replay")){
-			Application.LoadLevel("game");
+
+		if (System.IO.File.Exists("assets/levels/level" + (GlobalFlags.getLevel() + 1) + ".txt")){
+			//System.IO.File.ReadAllLines("assets/levels/level" + (GlobalFlags.getLevel() + 1) + ".txt");
+			//GlobalFlags.setLevel(GlobalFlags.getLevel() + 1);
+
+			float buttonW = 150;
+			float buttonH = 50;
+			
+			//set the play game button
+			float halfScreenW = (Screen.width/2) - 200;
+			float halfScreenH = (Screen.height/2) + 0;
+			if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Replay")){
+				Application.LoadLevel("game");
+			}
+
+			halfScreenW = (Screen.width/2) +50;
+			halfScreenH = (Screen.height/2) + 0;
+			if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Play Next")){
+				GlobalFlags.setLevel(GlobalFlags.getLevel() + 1);
+				Application.LoadLevel("game");
+			}
+			
+			buttonW = 150;
+			
+			//set the exit button
+			halfScreenW = (Screen.width/2) - 200;
+			halfScreenH = Screen.height/2 + 60;
+			if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Level Select")){
+				Application.LoadLevel("LevelSelectPreMenu");
+			}
+			
+			//set the exit button
+			halfScreenW = (Screen.width/2) + 50;
+			halfScreenH = Screen.height/2 + 60;
+			if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Main Menu")){
+				Application.LoadLevel("ModeSelectMenu");
+			}
+
 		}
+		else{
+			float buttonW = 100;
+			float buttonH = 50;
+			
+			//set the play game button
+			float halfScreenW = (Screen.width/2) - 50;
+			float halfScreenH = Screen.height/2 + 0;
+			if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Replay")){
+				Application.LoadLevel("game");
+			}
 
-		buttonW = 150;
+			buttonW = 150;
 
-		//set the exit button
-		halfScreenW = (Screen.width/2) - 200;
-		halfScreenH = Screen.height/2 + 60;
-		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Level Select")){
-			Application.LoadLevel("LevelSelectPreMenu");
-		}
+			//set the exit button
+			halfScreenW = (Screen.width/2) - 200;
+			halfScreenH = Screen.height/2 + 60;
+			if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Level Select")){
+				Application.LoadLevel("LevelSelectPreMenu");
+			}
 
-		//set the exit button
-		halfScreenW = (Screen.width/2) + 50;
-		halfScreenH = Screen.height/2 + 60;
-		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Main Menu")){
-			Application.LoadLevel("ModeSelectMenu");
+			//set the exit button
+			halfScreenW = (Screen.width/2) + 50;
+			halfScreenH = Screen.height/2 + 60;
+			if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Main Menu")){
+				Application.LoadLevel("ModeSelectMenu");
+			}
 		}
 	}
 }
