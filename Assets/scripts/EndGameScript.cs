ea09110e75cfd9644b849269f87a38a6d1b3b34f
Jeremy Kooper <JeremyKooper24@gmail.com>
2014-04-06 22:54:02 -0400

    fixed scaling issues for the screen for when you lose

--- Assets/scripts/EndGameScript.cs
+++ Assets/scripts/EndGameScript.cs
@@ -10,23 +10,22 @@ public class EndGameScript : MonoBehaviour {
 	
 	private void Start(){
 		//back drop dimensions
-		float backW = 200;
-		float backH = 250;
+		float backW = 3 * Screen.width/4;
+		float backH = Screen.height/2;
 		//center the back drop
 		float halfScreenW = (Screen.width/2) - backW/2;
 		float halfScreenH = (Screen.height/2) - backH/2;
 		
 		backWindow = new Rect(halfScreenW, halfScreenH, backW, backH);//build backdrop
 
-
 		//set the label dimensions for the 
-		backW = 50;
-		backH = 150;
+		backW = (Screen.width/2) - backW/2;
+		backH = (Screen.height/2) - backH/2;
 
 		halfScreenW = (Screen.width/2) - backW/2;
-		halfScreenH = (Screen.height/2) - backH/2;
+		halfScreenH = backH + Screen.height/11;
 
-		label = new Rect(halfScreenW, halfScreenH, 100, 50);
+		label = new Rect(0, halfScreenH, Screen.width, halfScreenH);
 	}
 	
 	void Update(){
@@ -34,15 +33,24 @@ public class EndGameScript : MonoBehaviour {
 	}
 	
 	void OnGUI(){
-
-		GUI.skin = skin;
-		GUI.Box(backWindow, "Game Over!");//title the menu
-		
+	
 		//button dimensions
-		float buttonW = 100;
-		float buttonH = 50;
-
-		GUI.Label(label, "Score: " + GlobalFlags.getScore());
+		float buttonW = Screen.width/2;
+		float buttonH = Screen.height/10;
+		
+		GUIStyle style = new GUIStyle(GUI.skin.button);
+		style.fontSize = (int)(buttonH / 2.5f);
+		
+		GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+		labelStyle.fontSize = (int)(buttonH / 2.5f);
+		labelStyle.alignment = TextAnchor.UpperCenter;
+		
+		GUI.skin = skin;
+		style.alignment = TextAnchor.UpperCenter;
+		GUI.Box(backWindow, "Game Over!", style);//title the menu
+		style.alignment = TextAnchor.MiddleCenter;
+		
+		GUI.Label(label, "Score: " + GlobalFlags.getScore(), labelStyle);
 		
 		// set the resume button and functionality
 		float halfScreenW = (Screen.width/2) - buttonW/2;
@@ -54,8 +62,8 @@ public class EndGameScript : MonoBehaviour {
 		
 		//set the restart button and functionality
 		halfScreenW = (Screen.width/2) - buttonW/2;
-		halfScreenH = Screen.height/2 - 20;
-		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Restart")){
+		halfScreenH = backWindow.y + Screen.height/5;
+		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Restart", style)){
 			//GlobalFlags.canFire = true;
 			Application.LoadLevel("game");
 			GlobalFlags.setPaused(false);
@@ -63,8 +71,8 @@ public class EndGameScript : MonoBehaviour {
 		
 		//set the back to menu button and functionality
 		halfScreenW = (Screen.width/2) - buttonW/2;
-		halfScreenH = Screen.height/2 + 40;
-		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Menu")){
+		halfScreenH = Screen.height/2 + buttonH * 1.2f;
+		if (GUI.Button(new Rect(halfScreenW,halfScreenH,buttonW,buttonH),"Back To Menu", style)){
 			//GlobalFlags.canFire = true;
 			Application.LoadLevel("LevelSelectPreMenu");
 			GlobalFlags.setPaused(false);
